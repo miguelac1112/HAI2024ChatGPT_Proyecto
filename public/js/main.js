1be19bebc524636084fa62569731f33290b80a35
Joaquin Orellana <joaquin.orellana@pucp.edu.pe>
2024-03-16 08:52:51 -0500

    Avance humilde fondo de pantlla 2 xd

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
 import * as avatar from "./avatar.js"
 import buttonBehavior from "./buttonBehavior.js";
 
+// Seleccionar el canvas por su ID (o cualquier otro selector)
+const canvas = document.getElementById('live2d');
+
+// Modificar el ancho y alto del canvas
+canvas.width = 1600;
+canvas.height = 700;
